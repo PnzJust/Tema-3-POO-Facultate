8163face62bfc6e801b372924769dce4d06c91fc
PnzJust <32747902+PnzJust@users.noreply.github.com>
2019-05-25 16:38:11 +0300

    Update tema.cpp

--- tema.cpp
+++ tema.cpp
@@ -114,19 +114,48 @@ class MONOVOLUMELE:public autoturism
 
 template <class T>
 class vanzare{
-
 	static unsigned nr_total_masini;
 	static unsigned nr_masini_vandute;
-	std::list<autoturism*> masini_vandute; 
-	std::list<autoturism*> masini_ramase; 
+	std::list<T*> masini_vandute; 
+	std::list<T*> masini_ramase; 
 public:
 	vanzare(){}
-	friend void operator -=(vanzare &X, T &ob)
+	friend void operator -=(vanzare &X, MINI &ob)
 	{
 		X.nr_total_masini--; X.nr_masini_vandute++;
-		autoturism *p=&ob;
+		T *p=&ob;
 		X.masini_vandute.push_back(p);
-		std::list<autoturism*>::iterator it;
+		typename std::list<T*>::iterator it;
+		for(it=X.masini_ramase.begin();it!=X.masini_vandute.end();it++)
+			if(*it==ob)
+				{X.masini_ramase.erase(it);break;}
+	}
+	friend void operator -=(vanzare &X, MICA &ob)
+	{
+		X.nr_total_masini--; X.nr_masini_vandute++;
+		T *p=&ob;
+		X.masini_vandute.push_back(p);
+		typename std::list<T*>::iterator it;
+		for(it=X.masini_ramase.begin();it!=X.masini_vandute.end();it++)
+			if(*it==ob)
+				{X.masini_ramase.erase(it);break;}
+	}
+	friend void operator -=(vanzare &X, COMPACTA &ob)
+	{
+		X.nr_total_masini--; X.nr_masini_vandute++;
+		T *p=&ob;
+		X.masini_vandute.push_back(p);
+		typename std::list<T*>::iterator it;
+		for(it=X.masini_ramase.begin();it!=X.masini_vandute.end();it++)
+			if(*it==ob)
+				{X.masini_ramase.erase(it);break;}
+	}
+	friend void operator -=(vanzare &X, MONOVOLUMELE &ob)
+	{
+		X.nr_total_masini--; X.nr_masini_vandute++;
+		T *p=&ob;
+		X.masini_vandute.push_back(p);
+		typename std::list<T*>::iterator it;
 		for(it=X.masini_ramase.begin();it!=X.masini_vandute.end();it++)
 			if(*it==ob)
 				{X.masini_ramase.erase(it);break;}
@@ -139,8 +168,11 @@ unsigned vanzare<T>::nr_total_masini=100;
 template <class T>
 unsigned vanzare<T>::nr_masini_vandute=0;
 
+
+
 int main()
 {
+
 
 
 	return 0;
